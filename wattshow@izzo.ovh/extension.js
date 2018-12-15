1d86022065aa361feb14d7f85632cf1b6b9a3f4e
Tomas Janousek <tomi@nomi.cz>
2018-12-15 22:10:30 +0100

    Only enable the timer once
    Previously we called _enable from both _init (through _refresh) and
    enable, setting up two timers but only ever disabling one of those,
    causing the other to leak, fill log with errors and eventually slow
    gnome-shell down considerably.
    
    Instead, use just one timeout_add and return true from _refresh to make
    the timer fire repeatedly, and only keep one timer in this.interval.

--- wattshow@izzo.ovh/extension.js
+++ wattshow@izzo.ovh/extension.js
@@ -69,19 +69,22 @@ WattMeter.prototype =
             power=power.toFixed(2);
             power_text=power.toString();
             temp.set_text(power_text+'w');
-            this._enable();
 
+            return true;
         },
 
         _enable: function()
         {
-            this.interval = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 5000,
-                                        Lang.bind(this, this._refresh));
+            if (!this.interval) {
+                this.interval = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 5000,
+                                            Lang.bind(this, this._refresh));
+            }
         },
 
         _disable: function()
         {
             GLib.source_remove(this.interval)
+            this.interval = null;
         }
 }
 
